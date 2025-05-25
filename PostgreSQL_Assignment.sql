e3ca24eb8a1df9f35c73a1d23b3a378e755f4716
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-05-25 22:27:45 +0600

    table species has been created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -18,3 +18,46 @@ VALUES (
     );
 ---------------------------
 SELECT * from rangers;
+
+CREATE TABLE species (
+    species_id serial PRIMARY KEY,
+    common_name VARCHAR(75) NOT nULL,
+    scientific_name VARCHAR(75) not NULL,
+    discovery_date DATE not NULL,
+    conservation_status VARCHAR(50) not null
+);
+
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+--
+-------------------------------------------
+select * from species;
