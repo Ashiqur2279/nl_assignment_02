2e1275e7c43bb5206a3a2374ac8fe767421e529c
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-05-26 09:08:39 +0600

    Add sightings table, insert sample data and solved the problems 1 to 6.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,4 @@
+--table rangers--------------------
 CREATE TABLE rangers (
     ranger_id serial PRIMARY KEY,
     name VARCHAR(100) not NULL,
@@ -16,9 +17,8 @@ VALUES (
         'Carol King',
         'Mountain Range'
     );
----------------------------
-SELECT * from rangers;
 
+--table species -----------------------------
 CREATE TABLE species (
     species_id serial PRIMARY KEY,
     common_name VARCHAR(75) NOT nULL,
@@ -58,6 +58,89 @@ VALUES (
         '1758-01-01',
         'Endangered'
     );
---
--------------------------------------------
-select * from species;
+
+-----------table sightings---------------------------
+CREATE TABLE sightings (
+    sighting_id serial PRIMARY KEY,
+    species_id INTEGER REFERENCES species (species_id),
+    ranger_id INTEGER REFERENCES rangers (ranger_id),
+    location VARCHAR(50) not NULL,
+    sighting_time TIMESTAMP not NULL,
+    notes text
+);
+
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
+
+--problem 01
+INSERT INTO
+    rangers ("name", region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+SELECT * FROM rangers;
+
+--problem 02
+SELECT count(DISTINCT species_id) as unique_species_count
+FROM sightings
+
+-- problem 03
+SELECT * FROM sightings WHERE "location" LIKE '%Pass%';
+
+--problem 04
+
+SELECT r.name, count(s.ranger_id) as total_sightings
+FROM rangers as r
+    JOIN sightings as s on r.ranger_id = s.ranger_id
+GROUP BY
+    r.name
+ORDER BY r.name;
+
+--problem 05
+SELECT sp.common_name
+from species as sp
+    LEFT join sightings as si on sp.species_id = si.species_id
+WHERE
+    si.species_id IS NULL;
+
+--problem 06
+SELECT sp.common_name, si.sighting_time, r.name
+FROM
+    species as sp
+    join sightings as si on sp.species_id = si.species_id
+    JOIN rangers as r on r.ranger_id = si.ranger_id
+ORDER BY si.sighting_time DESC
+LIMIT 2;
